0d948ee3cbd6719613603bd1fa36b60cd03b6fa3
Kashan Aqeel <kashanaqeel74@gmail.com>
2023-07-07 21:17:20 +0500

    social-media-icons-added

--- sustainerzz/src/layout/Footer.js
+++ sustainerzz/src/layout/Footer.js
@@ -4,10 +4,20 @@ export default function Footer() {
   return (
     <div>
     <ul>
-        <li><a href=""></a></li>
-        <li><a href=""></a></li>
-        <li><a href=""></a></li>
-        <li><a href=""></a></li>
+    <div className="social-icons">
+       <a href="https://www.youtube.com">
+          <i className="fab fa-youtube"></i>
+        </a>
+        <a href="https://www.instagram.com">
+          <i className="fab fa-instagram"></i>
+        </a>
+        <a href="https://www.gmail.com">
+          <i className="far fa-envelope"></i>
+        </a>
+        <a href="https://www.linkedin.com">
+          <i className="fab fa-linkedin"></i>
+        </a>
+      </div>
         <li><a className="active" href=""></a></li>
         <p className="copyright">
         Copyright &copy; 2023 Designed by Sustainerzz.
