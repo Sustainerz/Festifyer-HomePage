d2754692cf457482cb339ff747d58ccddf315b7c
RemalFatima <f201103@cfd.nu.edu.pk>
2023-07-07 07:35:50 -0700

    Add Footer

---
+++ sustainerzz/src/layout/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+export default function Footer() {
+  return (
+    <div>
+    <ul>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a className="active" href=""></a></li>
+      </ul>
+</div>
+  )
+}
