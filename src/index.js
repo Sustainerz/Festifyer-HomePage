6c9d4788145a79e0af4f3bb124b40d55bf769c9e
RemalFatima <f201103@cfd.nu.edu.pk>
2023-07-07 13:49:47 -0700

    Modified Social Icons

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Footer/>
+    <Footer DevName= 'Sustainerzz'></Footer>
       </React.StrictMode>
 );
