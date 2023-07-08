353d687fac53f389f989faa0f2fa741ddd7fe6bd
RemalFatima <f201103@cfd.nu.edu.pk>
2023-07-08 02:59:22 -0700

    Modified Social Icons

---
+++ src/layout/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faYoutube, faInstagram, faGoogle, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+export default function Footer() {
+  return (
+    <div>
+    <ul>
+    <div className="social-icons">
+       <a href="https://www.youtube.com">
+       <FontAwesomeIcon icon={faYoutube} className="youtube" />
+        </a>
+        <a href="https://www.gmail.com">
+        <FontAwesomeIcon icon={faGoogle} className="gmail" />
+        </a>
+        <a href="https://www.instagram.com">
+        <FontAwesomeIcon icon={faInstagram} className="instagram" />
+        </a>
+        <a href="https://www.linkedin.com">
+        <FontAwesomeIcon icon={faLinkedin} className="linkedin" />
+        </a>
+      </div>
+        <li><a className="active" href=""></a></li>
+        <p className="copyright">
+        Copyright &copy; 2023 Designed by Sustainerzz.
+      </p>
+      </ul>
+</div>
+  )
+}
